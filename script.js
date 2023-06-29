85259014bdd492627b231ae2938b3698f80cf918
Ola <hollabodey.kilaso@gmail.com>
2023-06-29 05:55:30 +0100

    Made changes to the index and css files

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let toggle = document.querySelector('label');
 
+
+
+
+const input = document.querySelector('input'),
+    number = document.querySelector('.views');
+    input.value = 0;
+input.addEventListener('input', () => {
+    number.textContent = input.value;
+});
+
 toggle.addEventListener('click', (e) => {
     e.preventDefault();
 
     toggle.classList.toggle('active');
 });
+
