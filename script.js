0d73655135c8b54488efd25ee711c73d3271e944
Ola <hollabodey.kilaso@gmail.com>
2023-06-29 20:54:19 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let toggle = document.querySelector('label');
 
 const input = document.querySelector('input'),
-number = document.querySelector('.views');
+    number = document.querySelector('.views');
 
 input.value = 0;
+
 input.addEventListener('input', () => {
     number.textContent = input.value;
+
 });
 
 toggle.addEventListener('click', (e) => {
